b5e2d8697bf60a8b7be4c90b456a670eff583e39
Jason Rothgeb <jrothgeb@gmail.com>
2022-08-02 15:24:45 -0500

    commit tree 1e1ca8daa7548e68ee2e0ea902012bb9d7877ec7

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  This was the solution that I had originally though of. 
+  I wasnt' sure of how to get the string length to match if the string had an even or odd number of characters, though.
 */
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
